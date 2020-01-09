08d1445c708613dc951450c4a430a931f5ac1202
jlalis <58454815+jlalis@users.noreply.github.com>
2020-01-09 11:50:19 +0800

    Add files via upload

---
+++ Lalis_010920_Task6.sql
@@ -0,0 +1,2 @@
+SELECT emp_lname,emp_fname,emp_initial
+FROM "010920".employee WHERE emp_jobcode = 'TECH'
---
+++ Lalis_010920_Task7.sql
@@ -0,0 +1,2 @@
+SELECT emp_jobcode, dept_code, prof_high_degree
+FROM "010920".professor,"010920".employee  WHERE dept_code = 'CIS'
